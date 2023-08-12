4c77ef445a9ca140a88987ff2b7c7ba51f1afc1a
Shodmon0406 <133884946+Shodmon0406@users.noreply.github.com>
2023-08-12 18:03:13 +0500

    relise notes

--- Problem_3/Program.cs
+++ Problem_3/Program.cs
@@ -3,3 +3,10 @@ var result = (from l in list
     where l.StartsWith("A") && l.EndsWith("A")
     select l).ToList();
 result.ForEach(Console.WriteLine);
+
+// metonistem ki rangi poyon knem neki sharty masala boloya talab kardest 
+/*var list = new List<string>() { "Alice", "Anny", "Amy", "Dave", "Alex" };
+var result = (from l in list
+    where l.StartsWith("A") && l.ToUpper().EndsWith("A")
+    select l).ToList();
+result.ForEach(Console.WriteLine);*/
